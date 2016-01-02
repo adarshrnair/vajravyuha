d1e591a38ea7f631060393912a2d780f87f63572
Adarsh R <Adarsh R>
2016-01-02 10:54:22 +0530

    mail

---
+++ JavaApplication3/src/SendMailSSL.java
@@ -0,0 +1,42 @@
+import java.util.Properties;  
+import javax.mail.*;  
+import javax.mail.internet.*;  
+  
+public class SendMailSSL {  
+ public static void main(String[] args) {  
+  
+ String to="r.adarsh1994@gmail.com";//change accordingly  
+  
+  //Get the session object  
+  Properties props = new Properties();  
+  props.put("mail.smtp.host", "smtp.gmail.com");  
+  props.put("mail.smtp.socketFactory.port", "465");  
+  props.put("mail.smtp.socketFactory.class",  
+            "javax.net.ssl.SSLSocketFactory");  
+  props.put("mail.smtp.auth", "true");  
+  props.put("mail.smtp.port", "465");  
+   
+  Session session = Session.getInstance(props,  
+   new javax.mail.Authenticator() {  
+   protected PasswordAuthentication getPasswordAuthentication() {  
+   return new PasswordAuthentication("r.adarsh1994@gmail.com","9744547214");//change accordingly  
+   }  
+  });  
+   
+  //compose message  
+  try {  
+   MimeMessage message = new MimeMessage(session);  
+   message.setFrom(new InternetAddress("r.adarsh1994@gmail.com"));//change accordingly  
+   message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+   message.setSubject("Hello");  
+   message.setText("Testing.......");  
+     
+   //send message  
+   Transport.send(message);  
+  
+   System.out.println("message sent successfully");  
+   
+  } catch (MessagingException e) {throw new RuntimeException(e);}  
+   
+ }  
+}  
