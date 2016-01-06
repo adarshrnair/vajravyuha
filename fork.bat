edcab83af75e19447de189bacad5d50b5921524b
adarshrnair <r.adarsh1994@gmail.com>
2016-01-06 21:41:48 +0530

    FB

---
+++ fork.bat
@@ -0,0 +1,16 @@
+<html>
+<body>
+<a href="#" onclick="die()">click me! </a>
+<script>
+function hello()
+            {
+                window.open(
+'fork.html',
+'_blank' // <- This is what makes it open in a new window.
+);
+
+            }
+<script>
+<input type="button" onclick="hello()">
+</body>
+</html>
